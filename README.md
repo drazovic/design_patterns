bb130ee7f2d6e29098cbccadbed139b2f57ad7f7
unknown <idrazovic@gmail.com>
2020-06-28 15:09:00 +0200

    singleton added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Design patterns written in Typescript 
+
+# Steps to run examples:
+
+0) Run "npm install -g typescript" - installs typescript globaly
+1) Run "tsc" - compiles all ts files to /dist folder
+2) Run "node dist/X" - where X is the name of the pattern file in compiled dist folder, i.e. "node dist/builder"
---
+++ src/creational/singleton.ts
@@ -0,0 +1,37 @@
+class AnimalDataService {
+    private static instance: AnimalDataService;
+    private data: string[] = [];
+
+    private constructor() {}
+
+    static getInstance(): AnimalDataService {
+        if (!AnimalDataService.instance) {
+            AnimalDataService.instance = new AnimalDataService();
+        }
+
+        return AnimalDataService.instance;
+    }
+
+    addAnimal(item: string): void {
+        this.data.push(item);
+    }
+
+    getAnimals(): string[] {
+        return this.data;
+    }
+}
+
+function compareAnimalServiecInstances() {
+    const animalService1 = AnimalDataService.getInstance();
+    animalService1.addAnimal('dog');
+
+    const animalService2 = AnimalDataService.getInstance();
+    animalService2.addAnimal('cat');
+
+    const animals1= animalService1.getAnimals();
+    const animals2 = animalService2.getAnimals();
+    console.log('Animals from animalService1', animals1);
+    console.log('Animals from animalService2', animals2);
+}
+
+compareAnimalServiecInstances();
